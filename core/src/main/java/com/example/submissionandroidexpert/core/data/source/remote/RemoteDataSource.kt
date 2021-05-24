85be4a72484b2b796749ecbf3571249f46fb7eb8
emce <michelle.ch211@gmail.com>
2021-05-24 13:05:33 +0700

    fix tag

--- core/src/main/java/com/example/submissionandroidexpert/core/data/source/remote/RemoteDataSource.kt
+++ core/src/main/java/com/example/submissionandroidexpert/core/data/source/remote/RemoteDataSource.kt
@@ -93,6 +93,6 @@ class RemoteDataSource constructor(private val api: MovieListAPI) {
     }
 
     companion object {
-        private const val TAG = "=========== REMOTE DATA SOURCE"
+        private const val TAG = "REMOTE DATA SOURCE"
     }
 }
