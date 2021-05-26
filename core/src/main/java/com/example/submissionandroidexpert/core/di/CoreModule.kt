33add1df7afcec60f1ba707a6ec3768167d8e4e0
emce <michelle.ch211@gmail.com>
2021-05-27 06:43:11 +0700

    fix cert pinning

--- core/src/main/java/com/example/submissionandroidexpert/core/di/CoreModule.kt
+++ core/src/main/java/com/example/submissionandroidexpert/core/di/CoreModule.kt
@@ -39,6 +39,9 @@ val networkModule = module {
         val hostname = "themoviedb.org"
         val certificatePinner = CertificatePinner.Builder()
             .add(hostname, "sha256/+vqZVAzTqUP8BGkfl88yU7SQ3C8J2uNEa55B7RZjEg0=")
+            .add(hostname, "sha256/JSMzqOOrtyOT1kmau6zKhgT676hGgczD5VMdRMyJZFA=")
+            .add(hostname, "sha256/++MBgDH5WGvL9Bcn5Be30cRcL0f5O+NyoXuWtQdX1aI=")
+            .add(hostname, "sha256/KwccWaCgrnaw6tsrrSO61FgLacNgG2MMLq8GE6+oP5I=")
             .build()
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
